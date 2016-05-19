3beda91c04f0bbc3926fbd0f199da1625c5b438b
pje335_NL <meulenthomas@gmail.com>
2016-05-19 16:06:21 +0200

    update6

--- opdrachten/week4/tabel.c
+++ opdrachten/week4/tabel.c
@@ -3,9 +3,9 @@
 #define colom 5
 #define N 32
 
+int data_1d[N] = {0},data_2d[vragen][colom]={{0}},i,j,l,k=2;
 
-int data_1d[N] = {0},data_2d[vragen][colom]={0},i,j,l,k=2;
-
+//function to calculate the averige of the question
 float averige(j){
   float sum=0;
   for(l=0;l < colom;l++){
@@ -24,7 +24,7 @@ for(i=0;i<N;i++){
 }
 fclose(fp);
 
-//1d naar 2d omzetten
+//confurting 1d to 2d array
 
 for(i=0;i<vragen;i++){
   for (j = 0; j < colom; j++) {
@@ -33,7 +33,7 @@ for(i=0;i<vragen;i++){
   }
 }
 
-
+//priting the static lines
 printf("Aantal uitgereikte evalaties:\t%d\n",data_1d[0] );
 printf("Aantal ingenomen evalaties:\t%d\n",data_1d[1] );
 printf("Response is: \t%.1f%%\n",(float)data_1d[1]/(float)data_1d[0]*100 );
@@ -41,12 +41,15 @@ printf("\n" );
 printf("\tA \t B\t C\t D\t E\tCijfer\topmerking\n" );
 
 
+//printing the lines per question
 for (i = 0; i < vragen; i++) {
   printf("vraag %d\t",(i+1) );
   for (j = 0; j < colom; j++) {
     printf("%d\t ", data_2d[i][j]);
   }
   printf("%.1f\t",averige((float)i) );
+
+  //giving feedback on the averige.
   if( averige((float)i) < 3.0){
     printf("voldoet niet!\n" );
   }
